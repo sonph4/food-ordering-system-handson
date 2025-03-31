7fe9b9193c2edf1f038bc40b2d519193d6e15e08
sonpham <thewalkingdeada@gmail.com>
2025-03-31 16:01:35 +0700

    sets up outbox - create outbox tables, create model, create repositories, create publisher

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/PaymentOutboxRepository.java
@@ -0,0 +1,21 @@
+package com.food.ordering.system.order.service.domain.ports.output.repository;
+
+import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import com.food.ordering.system.outbox.OutboxStatus;
+import com.food.ordering.system.saga.SagaStatus;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PaymentOutboxRepository {
+    OrderPaymentOutboxMessage save (OrderPaymentOutboxMessage outboxMessage);
+    Optional<OrderPaymentOutboxMessage> findByTypeAndOutboxStatusAndSagaStatus (String type,
+                                                                                 OutboxStatus outboxStatus,
+                                                                                 SagaStatus... sagaStatuses);
+    Optional<OrderPaymentOutboxMessage> findByTypeAndSagaIdAndSagaStatus(String type,
+                                                                         UUID sagaId,
+                                                                         SagaStatus... sagaStatuses);
+    void deleteByTypeAndOutboxStatusAndSagaStatus(String type,
+                                                  OutboxStatus outboxStatus,
+                                                  SagaStatus... sagaStatuses);
+}
